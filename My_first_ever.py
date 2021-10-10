57c68cf3c59bef6678a6af89ab568c3dc7dd245f
Mirza Usama Haider <mirzausamahaider@gmail.com>
2021-10-10 02:50:06 +0100

    2nd commit

--- My_first_ever.py
+++ My_first_ever.py
@@ -1,16 +1,31 @@
-age= 22
-first_name="Mirza"
-is_alive = False
+#---------------------------------------------------------------------------------
+#age= 22
+#first_name="Mirza"
+#is_alive = False
 
-print(age)
+#print(age)
 
-print("Hello, World!")
+#print("Hello, World!")
 
-first_name = "John"
-last_name= "Smith"
-age = 20
-is_registered =  False
+#first_name = "John"
+#last_name= "Smith"
+#age = 20
+#is_registered =  False
 
-name = input("What is your name? ")
-print("Hello "+ name +" Are you happy with it? ")
+#name = input("What is your name? ")
+#print("Hello "+ name +" Are you happy with it? ")
+#------------------------------------------------------------------------------------
 
+#birth_year = input("What is your birth year ? ")
+#age = 2021 - int(birth_year)
+#print(age)
+
+##--------------------ADD----------
+
+first_number = input("First Number: ")
+
+second_number = input("Second Number: ")
+
+sum = float(first_number) + float(second_number)
+
+print("Sum: " + str(sum))
