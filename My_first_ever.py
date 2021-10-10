97a6c62c5b5d197ad57711c27a70b4c3871a37fd
Mirza Usama Haider <mirzausamahaider@gmail.com>
2021-10-10 02:14:05 +0100

    First Commit

---
+++ My_first_ever.py
@@ -0,0 +1,16 @@
+age= 22
+first_name="Mirza"
+is_alive = False
+
+print(age)
+
+print("Hello, World!")
+
+first_name = "John"
+last_name= "Smith"
+age = 20
+is_registered =  False
+
+name = input("What is your name? ")
+print("Hello "+ name +" Are you happy with it? ")
+
